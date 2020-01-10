dab8037e54dc597894b0d8c329cfe93e100ac683
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-01-10 23:44:14 +0530

    lettuce: Kill baseband assertion
    bitch please, we have volte modem too

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,9 +23,6 @@ DEVICE_PATH := device/yu/lettuce
 # Audio
 USE_XML_AUDIO_POLICY_CONF := 1
 
-# Assertions
-TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
-
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 
